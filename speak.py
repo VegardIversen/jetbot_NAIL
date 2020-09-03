c06f4ae44310d570eda8103d9d551bf5233c9194
VegardIversen <48367792+VegardIversen@users.noreply.github.com>
2020-09-03 15:16:00 +0200

    Delete speak.py

--- speak.py
+++
@@ -1,7 +0,0 @@
-from gtts import gTTS
-import playsound
-def speak(text, language='en'):
-    tts = gTTS(text=text, lang=language)
-    filename = 'voice1.mp3'
-    tts.save(filename)
-    playsound.playsound(filename)
